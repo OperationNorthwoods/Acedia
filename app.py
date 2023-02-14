31b08241aeaa63bb3173520123b0f1a068e579d5
Luke <87561450+OperationNorthwoods@users.noreply.github.com>
2023-02-14 16:50:34 -0500

    completely broke it
    trying to copy and paste random stuff from the wtforms docs. need to study them more deeply

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ import os
 from flask import Flask, render_template
 from config import API_KEY
 from markupsafe import escape
+import requests
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 
@@ -9,10 +13,20 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/search')
-def search_user():
-    print('api stuffs')
 
+class SteamSearch(FlaskForm):
+    name = StringField('name', validators=[DataRequired()])
+
+@app.route('/submit', methods=['GET', 'POST'])
+def submit():
+    form = SteamSearch()
+    if form.validate_on_submit():
+        return render_template('/results.html')
+    return render_template('index.html', form=form)
+
+@app.route('/results')
+def results():
+    return render_template('result.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
